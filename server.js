3c5f59259b9e32ab19dcd841ee8810b5a1909fc8
z1kary <vicburet@orange.fr>
2022-12-10 19:30:52 +0100

    commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
   'preflightContinue': false
 }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(bodyParser.json())
